86fa8e6657e026e83f8e9569ee329cbb15a5de5a
luisa.jardim <luisa.jardim@inspirali.com>
2025-08-04 10:40:42 -0300

    Refatora execução de scripts para utilizar módulos em vez de caminhos absolutos

--- run_all.py
+++ run_all.py
@@ -1,14 +1,13 @@
 import subprocess
 import sys
-import os
 
 # Executa a indexação
 print('Iniciando indexação dos dados...')
-ret = subprocess.run([sys.executable, os.path.join('codificacao', 'indexar_tudo.py')])
+ret = subprocess.run([sys.executable, "-m", "codificacao.indexar_tudo"])
 if ret.returncode != 0:
     print('Erro na indexação. Abortando.')
     sys.exit(1)
 
 # Executa a interface adaptativa
 print('Iniciando interface adaptativa...')
-subprocess.run([sys.executable, os.path.join('codificacao', 'prompt_adaptativo_gui.py')])
+subprocess.run([sys.executable, "-m", "codificacao.prompt_adaptativo_gui"])
